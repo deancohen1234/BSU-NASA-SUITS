44e8d97ac4d62a4aaa0cd4ab93bfbc36a0964a03
Unknown <dean3712@gmail.com>
2018-03-05 17:15:49 -0700

    Added voice control and JSON parsing

---
+++ VoiceControlProject/Assets/JSONParse.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JSONParse : MonoBehaviour {
+
+    public string m_JSONString;
+	// Use this for initialization
+	void Start ()
+    {
+        //NASADataType jsonObject = JsonUtility.FromJson<NASADataType>(m_JSONString);
+        NASADataType jsonObject = JsonUtility.FromJson<NASADataType>(m_JSONString);
+        //Debug.Log(jsonObject);
+        Debug.Log(jsonObject.p_sop);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
+
+[System.Serializable]
+public class NASADataType
+{
+    public int p_sub = 0;
+    public int t_sub = 0;
+    public int v_fan = 0;
+    public string t_eva = "";
+    public int p_o2 = 0;
+    public float rate_o2 = 0.0f;
+    public int cap_battery = 0;
+    public int p_h2o_g = 0;
+    public int p_h2o_l = 0;
+    public int p_sop = 0;
+    public float rate_sop = 0.0f;
+}
+
+public class TestClass
+{
+    public string _id;
+}
---
+++ VoiceControlProject/Assets/VoiceManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class VoiceManager : MonoBehaviour {
+
+	KeywordRecognizer keywordRecognizer;
+	Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+	// Use this for initialization
+	void Start () 
+	{
+	//Create keywords for keyword recognizer
+		keywords.Add("computer", () =>
+		{
+				Debug.Log("I understand and hear you! God you are annoying");
+			// action to be performed when this keyword is spoken
+		});
+
+		keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+		keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+
+		keywordRecognizer.Start ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+	{
+		Debug.Log ("Key Word Recognized");
+		System.Action keywordAction;
+		// if the keyword recognized is in our dictionary, call that Action.
+		if (keywords.TryGetValue(args.text, out keywordAction))
+		{
+			keywordAction.Invoke();
+		}
+	}
+}
