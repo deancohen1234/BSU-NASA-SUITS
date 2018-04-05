46f1ff81ff0a0dac93b54eac3d19c9be041fef9d
Unknown <dean3712@gmail.com>
2018-04-05 16:10:04 -0600

    Added back button and new menu to scene

--- Origami/Assets/Scripts/MenuController.cs
+++ Origami/Assets/Scripts/MenuController.cs
@@ -7,13 +7,30 @@ public class MenuController : MonoBehaviour
     public GameObject m_OGMenu;
     public GameObject m_NextMenu;
 
+    private GameObject m_CurrentMenu;
+    private GameObject m_PreviousMenu;
+
+    //hide old menu, and switch to new menu
 	public void ChangeMenu(GameObject oldMenu, GameObject newMenu)
     {
+        m_CurrentMenu = newMenu;
+        m_PreviousMenu = oldMenu;
+
         newMenu.transform.position = oldMenu.transform.position;
-        newMenu.transform.rotation = Camera.main.transform.rotation;
+        newMenu.transform.rotation = Quaternion.Euler(new Vector3(newMenu.transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, newMenu.transform.eulerAngles.z));
 
         oldMenu.SetActive(false);
         newMenu.SetActive(true);
+    }
+
+    //go back to previous menu
+    public void GoBack()
+    {
+        m_PreviousMenu.transform.position = m_CurrentMenu.transform.position;
+        m_PreviousMenu.transform.rotation = Quaternion.Euler(new Vector3(m_CurrentMenu.transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, m_PreviousMenu.transform.eulerAngles.z));
+
+        m_CurrentMenu.SetActive(false);
+        m_PreviousMenu.SetActive(true);
     }
 
     public void Update()
@@ -21,6 +38,10 @@ public class MenuController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             ChangeMenu(m_OGMenu, m_NextMenu);
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            GoBack();
         }
     }
 }
