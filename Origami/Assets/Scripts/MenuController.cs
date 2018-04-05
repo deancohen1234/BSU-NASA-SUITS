4259ed792843db73839193b0fde2c41dc70afc6c
Unknown <dean3712@gmail.com>
2018-04-05 15:41:10 -0600

    Beginnings of menu changing

---
+++ Origami/Assets/Scripts/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject m_OGMenu;
+    public GameObject m_NextMenu;
+
+	public void ChangeMenu(GameObject oldMenu, GameObject newMenu)
+    {
+        newMenu.transform.position = oldMenu.transform.position;
+        newMenu.transform.rotation = Camera.main.transform.rotation;
+
+        oldMenu.SetActive(false);
+        newMenu.SetActive(true);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeMenu(m_OGMenu, m_NextMenu);
+        }
+    }
+}
